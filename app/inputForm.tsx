cf7f9914e3769b83b82867c177fe21c35964319c
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-08-14 00:37:41 -0700

    collect inputform

--- app/inputForm.tsx
+++ app/inputForm.tsx
@@ -1,46 +1,49 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
-export default function InputForm(){
-    const [form, setForm] = useState('')
-    const [update, setUpdate] = useState('');
-    
-    return (
-      <div className={`mt-4 px-4`}>
-        <div>
-          {form}
-          <input
-            type="text "
-            placeholder="First Name"
-            value={form}
-            onChange={(e) => {
-              setForm(e.target.value);
-            }}
-            className={`bg-white border-white border-2 px-4 mb-4 text-black`}
-          />
+export default function InputForm() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [showName, setShowName] = useState(false);
+  return (
+    <div className={`mt-4 px-4`}>
+      <div>
+        <input
+          type="text "
+          placeholder="First Name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          className={`bg-white border-white border-2 px-4 mb-4 text-black`}
+        />
 
-          <input
-            type="text"
-            placeholder="Last Name"
-            value={update}
-            onChange={(e) => {
-              setUpdate(e.target.value);
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+          className={`bg-white border-white border-2 px-4 mb-4 text-black`}
+        />
+
+        <div className={`flex items-center justify-center`}>
+          <button
+            onClick={() => {
+              
+                setShowName(true);
+              
+              
             }}
-            className={`bg-white border-white border-2 px-4 mb-4 text-black`}
-          />
-          {update}
-          <div className={`flex items-center justify-center`}>
-            <button
-              onClick={() => {
-                setUpdate(update);
-                setForm(form);
-              }}
-            >
-              Submit
-            </button>
-          </div>
+          >
+            Submit
+          </button>
         </div>
+        {showName && name}
+        {showName && lastName}
       </div>
-    );
+    </div>
+  );
 }
