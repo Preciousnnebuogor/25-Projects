de9fd3a3f70650eb39b554e635b6db62f39426f3
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-08-20 23:07:14 -0700

    added time.tsx

--- app/nextPrevious.tsx
+++ app/nextPrevious.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+// Have two button, Next for nexting images while Previous button for previous images//
 import { useState } from "react";
 
 export default function NextPrevious() {
---
+++ app/time.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Time(){
+    const [time,setTime] = useState("")
+    const Day = ["monday","tuesday","wednesday","thurseday","friday","saturday","sunday"]
+    
+    return <div>
+     
+    </div>
+}
