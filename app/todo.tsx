dd11a8b01396c14f857d2f779d55aa742172d8b0
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-09-02 14:46:57 -0700

    added

--- app/todo.tsx
+++ app/todo.tsx
@@ -1,25 +1,42 @@
-"use client"
+"use client";
 
-
+import { useState } from "react";
 import { FaEyeDropper } from "react-icons/fa6";
 
-export default function TodoList(){
-    return (
-      <div className={`mt-10 px-4`}>
-        <div>
-          <div className={`flex items-center justify-center space-x-2`}>
-            <FaEyeDropper className={`text-orange-500`} />
-            <input type="text" className={`border-orange-500 border-b-4 bg-black`} />
-          </div>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
-              voluptatem ducimus, nulla repellat optio non corrupti. Porro ad
-              similique ab, sequi doloribus non voluptatum eos libero illo,
-              velit id ea?
-            </p>
-          </div>
+export default function TodoList() {
+  const [write, setWrite] = useState("");
+  const [add, setAdd] = useState<string[]>([]);
+  return (
+    <div className={`mt-10 px-4`}>
+      <div>
+        <div className={`flex items-center justify-center space-x-2`}>
+
+          <FaEyeDropper className={`text-orange-500`} />
+
+          <input
+            type="text"
+            value={write}
+            onChange={(e) => {
+              setWrite(e.target.value);
+            }}
+            className={`border-orange-500 border-b-4 bg-black mb-3`}
+          />
+
+          <button
+            onClick={() => {
+              setAdd([...add, write])
+              setWrite("");
+            }}
+          >
+            Add
+          </button>
+        </div>
+        <div className={`mt-4`}>
+          {add.map((value, index)=> (
+            <div>{value}</div>
+          ))}
         </div>
       </div>
-    );
+    </div>
+  );
 }
