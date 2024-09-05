40c73f16e19b472edcb9a3241e0a2696525b2559
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-09-04 21:13:42 -0700

    updated flashCard.tsx

--- app/flashCard.tsx
+++ app/flashCard.tsx
@@ -1,7 +1,13 @@
 "use client";
+
+import { useState } from "react";
+
 export default function FlashCard() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [List, setList] = useState<{ question: string; answer: string }[]>([]);
   return (
-    <div>
+    <div className={`bg-pink-600 h-full`}>
       <div
         className={`border-b-2 border-gray-500 flex items-center justify-between 
             px-4 pb-2 bg-slate-300 text-black pt-4`}
@@ -21,18 +27,50 @@ export default function FlashCard() {
 
           <div className={`flex flex-col my-4 `}>
             <label>Question</label>
-            <input type="text" className={`outline-none px-2`} />
+            <input
+              type="text"
+              onChange={(e) => {
+                setQuestion(e.target.value);
+              }}
+              value={question}
+              className={`outline-none px-2`}
+            />
           </div>
 
           <div className={`flex flex-col my-4 `}>
             <label>Answer</label>
-            <input type="text" className={`outline-none px-2`} />
+            <input
+              type="text"
+              onChange={(e) => {
+                setAnswer(e.target.value);
+              }}
+              value={answer}
+              className={`outline-none px-2`}
+            />
           </div>
 
           <div className={`flex items-center justify-start space-x-2`}>
-            <button>Save</button>
+            <button
+              onClick={() => {
+                setList([...List, { answer: answer, question: question }]);
+                setAnswer("");
+                setQuestion("");
+              }}
+            >
+              Save
+            </button>
             <button>Close</button>
           </div>
+        </div>
+      </div>
+      <div>
+        <div className={`flex items-center justify-start px-4 space-x-2`}>
+          {List.map((value, index) => (
+            <div className={`mt-4 bg-gray-300 rounded px-4 text-black `}>
+              <div className={` my-4`}>{value.question}</div>
+              <div className={` my-4`}>{value.answer}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
