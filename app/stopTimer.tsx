501c2c6fdf5b25fa4185109ae6a7203ff284c00c
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-09-06 11:43:46 -0700

    added stoptimer.tsx

---
+++ app/stopTimer.tsx
@@ -0,0 +1,12 @@
+"use client"
+export default function StopTimer(){
+    return (
+      <div className={`flex items-center justify-center`}>
+        <div className={``}>
+          <p>00:</p>
+          <p>00:</p>
+          <p>00:</p>
+        </div>
+      </div>
+    );
+}
