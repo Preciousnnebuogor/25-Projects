1b4476252fe5f5790f565641a20ca3d547f409d6
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-09-12 14:41:08 -0700

    added data.js

---
+++ app/data.ts
@@ -0,0 +1,54 @@
+
+
+type IProduct = {
+  image: string;
+  details: string;
+  link: string;
+};
+
+export const ProductList: IProduct[] = [
+  {
+    image: "/facebook1.jpg",
+    details: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quo
+            deserunt sed unde cupiditate, aperiam maxime odit porro quae
+            maiores! Itaque molestias assumenda voluptate hic dolorum aut labore
+            quod. Earum?`,
+    link: `https://www.facebook.com/precious.osemeke?mibextid=ZbWKwL`,
+  },
+
+  {
+    image: "/facebook2.jpg",
+    details: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quo
+            deserunt sed unde cupiditate, aperiam maxime odit porro quae
+            maiores! Itaque molestias assumenda voluptate hic dolorum aut labore
+            quod. Earum?`,
+    link: `https://github.com/Preciousnnebuogor`,
+  },
+
+  {
+    image: "/facebook3.jpg",
+    details: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quo
+            deserunt sed unde cupiditate, aperiam maxime odit porro quae
+            maiores! Itaque molestias assumenda voluptate hic dolorum aut labore
+            quod. Earum?`,
+    link: `https://www.instagram.com/preciousperfection1?utm_source=qr&igsh=eXZkMDJnOHhvZHho`,
+  },
+
+  {
+    image: "/facebook4.jpg",
+    details: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quo
+            deserunt sed unde cupiditate, aperiam maxime odit porro quae
+            maiores! Itaque molestias assumenda voluptate hic dolorum aut labore
+            quod. Earum?`,
+    link: `https://www.instagram.com/preciousperfection1?utm_source=qr&igsh=eXZkMDJnOHhvZHho`,
+  },
+
+  {
+    image: "/facebook5.jpg",
+    details: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quo
+            deserunt sed unde cupiditate, aperiam maxime odit porro quae
+            maiores! Itaque molestias assumenda voluptate hic dolorum aut labore
+            quod. Earum?`,
+    link: `https://x.com/home`,
+  },
+];
--- app/squareCard.tsx
+++ app/squareCard.tsx
@@ -1,8 +1,20 @@
-"use client"
-export default function SquareCard(){
-    return <div>
-        <div>
-            
+"use client";
+
+import { ProductList } from "./data";
+
+export default function SquareCard() {
+  return (
+    <div>
+      <div>
+     {ProductList.map((value,index)=>(
+        <div className={`flex flex-col items-center justify-between`}>
+        <div className={` bg-slate-500 rounded`}>
+        {value.image}
+        {value.details}
         </div>
+        </div>
+     ))}
+      </div>
     </div>
+  );
 }
