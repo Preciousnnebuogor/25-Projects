eca9e22127ff555f4a05aa979c8fc5bd2534a1ca
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-10-10 21:52:45 -0700

    added strings methods

--- app/arraryMethod.tsx
+++ app/arraryMethod.tsx
@@ -1,19 +1,69 @@
-"use client"
-export default function ArraryMethod(){
-const method =["john", "may", "cime", "money"]
-const method2 = ["johnny", "framay", "mocime", "enmoney"];
-//  const result = method.join();
-//  console.log(result);
- const result2 = method.concat(method2)
- console.log(result2)
- //method.splice(2,3)
- //method.splice(1,2)
- const name = method.slice(1,3)
- console.log(name)
- 
+"use client";
 
-   return <div>
-  <p>{`i am very okay ${method} so whate ${method2}`}</p>
+export default function ArraryMethod() {
+  let csvData = "name,age,location";
+  console.log(csvData.split(","));
+  let result = csvData;
 
-   </div>
+  //split()
+  let date = "2024-10-10";
+  console.log(date.split("-"));
+  let parte = date;
+
+  //trim()
+  let text = "NoSpasHere";
+  let space = text.trim();
+  console.log(space);
+
+  //startWith()
+  let word = "Hello, world!";
+  console.log(word.startsWith("Hello"));
+  console.log(word.startsWith("world"));
+
+  //repeat()
+  let access = "Hi! ";
+  let repeatedText = access.repeat(3);
+  console.log(repeatedText);
+
+  let stars = "*".repeat(5);
+  console.log(stars);
+
+  //match()
+  let sentence = "The sun sets in the west sun sun.";
+  let show = sentence.match(/sun/gi);
+  console.log(show);
+
+  let show2 = "John's phone number is 123-456-7890";
+  let numbers = show2.match(/\d+/g); // finds all numbers
+  console.log(numbers); // ["123", "456", "7890"]
+
+//search()
+  let come = "I love cats and dogs.";
+  let index = come.search(/cats|dogs/);
+  console.log(index); // 7 (position of "cats")
+
+  let come1 = "Learning JavaScript is fun!";
+  let index2 = come1.search("JavaScript");
+  console.log(index2);
+
+  //substring()
+  let text2 = "JavaScript";
+  let part = text2.substring(0, 4);
+  console.log(part);
+
+  //padStart
+  let text3 = "Hello";
+  let paddedText = text3.padStart(10);
+  console.log(paddedText);
+
+  let number = "5";
+  let paddedNumber = number.padStart(3, "0");
+  console.log(paddedNumber);
+
+  //padEnd()
+  let product = "Apple";
+  let paddedProduct = product.padEnd(10, ".");
+  console.log(paddedProduct);
+
+  return <div>{parte.split("-")}</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 
 import Checked from "./aaaaa";
+import ArraryMethod from "./arraryMethod";
 import PasswordGenerator from "./passwordGenerator";
 
 
 export default function Home() {
   return (
     <div>
-      <Checked />
+      <ArraryMethod />
     </div>
   );
 }
